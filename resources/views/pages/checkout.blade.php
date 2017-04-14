6d10c6af614e6be236fbb8e9799e11eed1fcbaf8
ishraqe <kanak431@diu.edu.bd>
2017-04-15 01:31:07 +0600

    chk out

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -12,7 +12,7 @@
 			  <li class="active">Check out</li>
 			</ol>
 		</div><!--/breadcrums-->
-
+		@if(Cart::count() !=0)
 		<div class="register-req">
 			<p> Returning customer ? <small>Click here to <a href="#">login</a></small></p>
 		</div><!--/register-req-->
@@ -23,11 +23,11 @@
 					<div class="bill-to">
 						<p>Bill To</p>
 						<div class="form-one">
-							<form>
+							
 								<input type="text" placeholder="Name">
 								<input type="text" placeholder="Address">
 								<input type="text" placeholder="Contact No">
-							</form>
+							
 						</div>
 						
 					</div>
@@ -35,15 +35,15 @@
 			</div>
 		</div>
 		<div class="review-payment">
-			<h2>Review & Payment</h2>
+			<h2>Review & Payment <small>(Cash on dalibery)</small></h2>
 		</div>
-
+		
 		<div class="table-responsive cart_info">
 			<table class="table table-condensed">
 				<thead>
 					<tr class="cart_menu">
 						<td class="image">Item</td>
-						<td class="description"></td>
+						<td class="description">Title</td>
 						<td class="price">Price</td>
 						<td class="quantity">Quantity</td>
 						<td class="total">Total</td>
@@ -51,89 +51,36 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td class="cart_product">
-							<a href=""><img src="images/cart/one.png" alt=""></a>
-						</td>
-						<td class="cart_description">
-							<h4><a href="">Colorblock Scuba</a></h4>
-							<p>Web ID: 1089772</p>
-						</td>
-						<td class="cart_price">
-							<p>$59</p>
-						</td>
-						<td class="cart_quantity">
-							<div class="cart_quantity_button">
-								<a class="cart_quantity_up" href=""> + </a>
-								<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-								<a class="cart_quantity_down" href=""> - </a>
-							</div>
-						</td>
-						<td class="cart_total">
-							<p class="cart_total_price">$59</p>
-						</td>
-						<td class="cart_delete">
-							<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-						</td>
-					</tr>
+					@foreach(Cart::content() as $row)
+                        <tr>
+                            <td class="cart_product">
+                                <a href="#"><img height="120px"   src="{{$row->options->image}}" alt=""></a>
+                            </td>
+                            <td class="cart_description">
+                                <p>{{$row->name}}</p>
 
-					<tr>
-						<td class="cart_product">
-							<a href=""><img src="images/cart/two.png" alt=""></a>
-						</td>
-						<td class="cart_description">
-							<h4><a href="">Colorblock Scuba</a></h4>
-							<p>Web ID: 1089772</p>
-						</td>
-						<td class="cart_price">
-							<p>$59</p>
-						</td>
-						<td class="cart_quantity">
-							<div class="cart_quantity_button">
-								<a class="cart_quantity_up" href=""> + </a>
-								<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-								<a class="cart_quantity_down" href=""> - </a>
-							</div>
-						</td>
-						<td class="cart_total">
-							<p class="cart_total_price">$59</p>
-						</td>
-						<td class="cart_delete">
-							<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-						</td>
-					</tr>
-					<tr>
-						<td class="cart_product">
-							<a href=""><img src="images/cart/three.png" alt=""></a>
-						</td>
-						<td class="cart_description">
-							<h4><a href="">Colorblock Scuba</a></h4>
-							<p>Web ID: 1089772</p>
-						</td>
-						<td class="cart_price">
-							<p>$59</p>
-						</td>
-						<td class="cart_quantity">
-							<div class="cart_quantity_button">
-								<a class="cart_quantity_up" href=""> + </a>
-								<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-								<a class="cart_quantity_down" href=""> - </a>
-							</div>
-						</td>
-						<td class="cart_total">
-							<p class="cart_total_price">$59</p>
-						</td>
-						<td class="cart_delete">
-							<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-						</td>
-					</tr>
+                            </td>
+                            <td class="cart_price">
+                                <p>$ <span class="value">{{$row->price}}</span></p>
+                            </td>
+                            <td class="cart_quantity">
+                                <p>{{$row->qty}}</p>
+                            </td>
+                            <td class="cart_total">
+                                <p id="totalPriceOf" class="cart_total_price">${{$row->qty*$row->price}}</p>
+                            </td>
+                            <td class="cart_delete">
+                                <a  href="{{route('cart.deleteItem',['rowId'=>$row->rowId])}}" class="cart_quantity_delete" onclick="removeItem(this)"><i class="fa fa-times"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
 					<tr>
 						<td colspan="4">&nbsp;</td>
 						<td colspan="2">
 							<table class="table table-condensed total-result">
 								<tr>
 									<td>Cart Sub Total</td>
-									<td>$59</td>
+									<td>${{Cart::subtotal()}}</td>
 								</tr>
 								<tr>
 									<td>Exo Tax</td>
@@ -145,7 +92,7 @@
 								</tr>
 								<tr>
 									<td>Total</td>
-									<td><span>$61</span></td>
+									<td><span>${{Cart::subtotal()}}</span></td>
 								</tr>
 								<tr>
 									<td>
@@ -160,11 +107,9 @@
 				</tbody>
 			</table>
 		</div>
-		<div class="payment-options">
-				<span>
-					<label> Cash on dalibery</label>
-				</span>
-			</div>
+		@else
+            <h2>No, items in cart</h2>
+        @endif
 	</div>
 </section> <!--/#cart_items-->
 @endsection	
